583acbec2d30b3efe24156ceeb2b90062f24cf0f
pll909@163.com <pll909@163.com>
2016-10-12 18:48:27 +0800

    update 10

--- js/common.js
+++ js/common.js
@@ -31,3 +31,49 @@ function getNextElement(node){
 
 	return null
 }
+
+function moveElement(elementId,final_x,final_y,interval){
+    if (!document.getElementById) { return false;};
+    if (!document.getElementById(elementId)) {return false;};
+    var elem = document.getElementById(elementId);
+    if (elem.movement) {
+    	clearTimeout(elem.movement);
+    }
+
+    if (!elem.style.left) {
+    	elem.style.left = '0px';
+    }
+    if (!elem.style.top) {
+    	elem.style.top = '0px';
+    }
+    var xpos = parseInt(elem.style.left);
+    var ypos = parseInt(elem.style.top);
+    
+    if (xpos == final_x && ypos == final_y) {
+        return true;
+    }
+
+    if (xpos < final_x) {
+    	dist = Math.ceil((final_x - xpos)/10);
+        xpos = xpos + dist;
+    }
+
+    if (xpos > final_x) {
+    	dist = Math.ceil((xpos - final_x)/10);
+       xpos = xpos - dist;
+    }
+
+    if (ypos < final_y) {
+    	dist = Math.ceil((final_y - ypos)/10);
+        ypos = ypos + dist;
+    }
+    if (ypos > final_y) {
+    	dist = Math.ceil((ypos - final_y)/10);
+        ypos = ypos - dist;
+    }
+
+    elem.style.left = xpos + "px";
+    elem.style.top = ypos + "px";
+    var repeat = "moveElement('"+elementId+ "',"+final_x+ "," +final_y+ "," +interval+ ")";
+    elem.movement = setTimeout(repeat,interval);
+}
